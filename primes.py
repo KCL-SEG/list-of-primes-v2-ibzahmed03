d36b6585809cdfdaa58d782044eadfca6ec07501
ibzahmed03 <iahmed1625@gmail.com>
2023-12-08 15:45:43 +0000

    completed task

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    current_num = 2
+
+    if number_of_primes <=0:
+        raise ValueError("NUmber of primes must be greater than 0")
+    
+    while count<number_of_primes:
+        if is_prime(current_num):
+            list.append(current_num)
+            count += 1
+        current_num +=1
+
     return list
+
+def is_prime(num):
+    if num<2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
